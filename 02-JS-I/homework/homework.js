e63f6ccc7ad658b4da256de3d41e272b5710bfdf
vanesa <vanesagabioud@yahoo.com.ar>
2022-02-14 20:16:39 -0300

    ejerc 4hecho

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -10,7 +10,7 @@ const nuevoNum = 24;
 const nuevoBool = false;
 
 // Resuelve el siguiente problema matemático:
-const nuevaResta = 10 - null === 5;
+const nuevaResta = 10 - 5=== 5;
 
 // Resuelve el siguiente problema matemático:
 const nuevaMultiplicacion = 10 * null === 40 ;
