40807b2f26c04ec8a7d477fd4ffb1b04fadade2b
vanesa <vanesagabioud@yahoo.com.ar>
2022-02-17 12:08:58 -0300

    cambio10

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -125,18 +125,20 @@ function elevarAlCuadrado(num) {
   // Devuelve el valor de "num" elevado al cuadrado
   // ojo: No es raiz cuadrada!
   // Tu código:
-  
+  return (num**2)
 }
 
 function elevarAlCubo(num) {
   // Devuelve el valor de "num" elevado al cubo
   // Tu código:
+  return (num**3)
   
 }
 
 function elevar(num, exponent) {
   // Devuelve el valor de "num" elevado al exponente dado en "exponent"
   // Tu código:
+  return num ** exponent
   
 }
 
