7f15f41a9bde2fabbb9339511ec5d00cb71ddc4b
vanesa <vanesagabioud@yahoo.com.ar>
2022-02-16 21:19:48 -0300

    cambio2

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -4,7 +4,7 @@
 const nuevaString = "hola";
 
 // Crea una variable numérica, puede ser cualquier número:
-const nuevoNum = null;
+const nuevoNum = "33";
 
 // Crea una variable booleana:
 const nuevoBool = null;
