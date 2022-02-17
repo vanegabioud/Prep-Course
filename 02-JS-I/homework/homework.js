9a8f96d4a92485276ed4ba49a34f7de943818040
vanesa <vanesagabioud@yahoo.com.ar>
2022-02-16 21:29:15 -0300

    cambio5

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -13,7 +13,7 @@ const nuevoBool = trueS;
 const nuevaResta = 10 - 5 === 5;
 
 // Resuelve el siguiente problema matemático:
-const nuevaMultiplicacion = null * 4 === 40 ;
+const nuevaMultiplicacion = 10 * 4 === 40 ;
 
 // Resuelve el siguiente problema matemático:
 const nuevoModulo = 21 % 5 === null;
